46245ed3fe97fd8364010518f87a5a1c1196f59a
Francis Bourre <francis.bourre@luxoffice.duodecadits.com>
2015-11-04 09:08:36 +0100

    [New] MockDependencyInjector

---
+++ test/hex/MockDependencyInjector.hx
@@ -0,0 +1,75 @@
+package hex;
+
+import hex.di.IDependencyInjector;
+
+/**
+ * ...
+ * @author Francis Bourre
+ */
+class MockDependencyInjector implements IDependencyInjector
+{
+	public function new() 
+	{
+		
+	}
+	
+	public function hasMapping( type : Class<Dynamic>, name : String = '' ) : Bool 
+	{
+		return false;
+	}
+	
+	public function hasDirectMapping( type : Class<Dynamic>, name:String = '' ) : Bool 
+	{
+		return false;
+	}
+	
+	public function satisfies( type : Class<Dynamic>, name : String = '' ) : Bool 
+	{
+		return false;
+	}
+	
+	public function injectInto( target : Dynamic ) : Void 
+	{
+		
+	}
+	
+	public function getInstance( type : Class<Dynamic>, name : String = '', targetType : Class<Dynamic> = null ) : Dynamic 
+	{
+		return null;
+	}
+	
+	public function getOrCreateNewInstance( type : Class<Dynamic> ) : Dynamic 
+	{
+		return null;
+	}
+	
+	public function instantiateUnmapped( type : Class<Dynamic> ) : Dynamic 
+	{
+		return null;
+	}
+	
+	public function destroyInstance( instance : Dynamic ) : Void 
+	{
+		
+	}
+	
+	public function mapToValue( clazz : Class<Dynamic>, value : Dynamic, ?name : String = '' ) : Void 
+	{
+		
+	}
+	
+	public function mapToType( clazz : Class<Dynamic>, type : Class<Dynamic>, name : String = '' ) : Void 
+	{
+		
+	}
+	
+	public function mapToSingleton( clazz : Class<Dynamic>, type : Class<Dynamic>, name : String = '' ) : Void 
+	{
+		
+	}
+	
+	public function unmap( type : Class<Dynamic>, name : String = '' ) : Void 
+	{
+		
+	}
+}
