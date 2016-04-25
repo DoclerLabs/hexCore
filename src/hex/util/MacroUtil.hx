0ff038166767c3a05d5fbf933b1f7c9faf6e9670
francis.bourre <francis_bourre@me.com>
2016-04-25 08:38:36 +0200

    MacroUtil class added

---
+++ src/hex/util/MacroUtil.hx
@@ -0,0 +1,32 @@
+package hex.util;
+
+import haxe.macro.Context;
+import haxe.macro.Expr.TypePath;
+
+/**
+ * ...
+ * @author Francis Bourre
+ */
+class MacroUtil
+{
+
+	function new() 
+	{
+		
+	}
+	
+	static public function getTypePath( className : String ) : TypePath
+	{
+		Context.getType( className );
+		var pack = className.split( "." );
+		var className = pack[ pack.length -1 ];
+		pack.splice( pack.length - 1, 1 );
+		return { pack: pack, name: className };
+	}
+	
+	static public function getPack( className : String ) : Array<String>
+	{
+		Context.getType( className );
+		return className.split( "." );
+	}
+}
