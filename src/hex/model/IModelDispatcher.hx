0b1938f1f6c8928f2f54f574bf24e6996c77fd9e
francis.bourre <francis_bourre@me.com>
2016-05-20 09:38:40 +0200

    IModelDispatcher is a typedef now instead of an interface

--- src/hex/model/IModelDispatcher.hx
+++ src/hex/model/IModelDispatcher.hx
@@ -3,9 +3,11 @@ package hex.model;
 /**
  * @author Francis Bourre
  */
-interface IModelDispatcher<ListenerType>
+typedef IModelDispatcher<ListenerType> = 
 {
-	function addListener( listener : ListenerType ) : Bool;
+	public function new() : Void;
+	
+	public function addListener( listener : ListenerType ) : Bool;
 
-	function removeListener( listener : ListenerType ) : Bool;
+	public function removeListener( listener : ListenerType ) : Bool;
 }
--- src/hex/model/ModelDispatcher.hx
+++ src/hex/model/ModelDispatcher.hx
@@ -5,7 +5,7 @@ package hex.model;
  * @author Francis Bourre
  */
 @:autoBuild( hex.model.ModelDispatcherAutoBuildMacro.build() )
-class ModelDispatcher<ListenerType> implements IModelDispatcher<ListenerType>
+class ModelDispatcher<ListenerType>
 {
 	var _listeners : Array<ListenerType>;
 	
