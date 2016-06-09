a27d06202b9e6af5296616b33ba802255b9256e4
francis.bourre <francis_bourre@me.com>
2016-06-09 16:34:04 +0200

    MonoTypeClosureDispatcher Fix for haxe 3.2.1 (more tests added)

--- src/hex/event/MonoTypeClosureDispatcher.hx
+++ src/hex/event/MonoTypeClosureDispatcher.hx
@@ -1,14 +1,20 @@
 package hex.event;
 
+#if ( haxe_ver >= "3.3" )
 import haxe.Constraints.Constructible;
+#end
 import hex.error.IllegalArgumentException;
 
 /**
  * ...
  * @author Francis Bourre
  */
+#if ( haxe_ver >= "3.3" )
 @:generic
 class MonoTypeClosureDispatcher<EventType:Constructible<String->Dynamic->Void>>
+#else
+class MonoTypeClosureDispatcher<EventType:Event>
+#end
 {
     var _eventType      	: String;
     var _target      		: Dynamic;
@@ -21,7 +27,11 @@ class MonoTypeClosureDispatcher<EventType:Constructible<String->Dynamic->Void>>
         this._callbacks         	= [];
     }
 
+	#if ( haxe_ver >= "3.3" )
     public function dispatchEvent( ?e : EventType ) : Void
+	#else
+	public function dispatchEvent( e : EventType ) : Void
+	#end
     {
 		#if ( haxe_ver >= "3.3" )
 		if ( e == null )
@@ -29,11 +39,16 @@ class MonoTypeClosureDispatcher<EventType:Constructible<String->Dynamic->Void>>
 			e = new EventType( this._eventType, this._target );
 		}
 		#else
-		if ( e != null && e.type != this._eventType )
+		if ( e == null )
 		{
-			throw new IllegalArgumentException( this + ".dispatchEvent failed. '" + e.type +"' should be '" + this._eventType + "'" );
+			throw new IllegalArgumentException( this + ".dispatchEvent failed." );
 		}
 		#end
+		
+		if ( e != null && (cast e).type != this._eventType )
+		{
+			throw new IllegalArgumentException( this + ".dispatchEvent failed. '" + (cast e).type +"' should be '" + this._eventType + "'" );
+		}
 		
 		for ( f in this._callbacks )
 		{
--- test/hex/event/MonoTypeClosureDispatcherTest.hx
+++ test/hex/event/MonoTypeClosureDispatcherTest.hx
@@ -31,7 +31,14 @@ class MonoTypeClosureDispatcherTest
     @After
     public function tearDown() : Void
     {
-        this._dispatcher    = null;
+        this._dispatcher = null;
+    }
+	
+	@Test( "Test dispatch method with illegal event type" )
+    public function testDispatchMethodWithIllegalEventType() : Void
+    {
+		var event = new BasicEvent( "anotherEvent", this._target );
+		Assert.methodCallThrows( IllegalArgumentException, this._dispatcher, this._dispatcher.dispatchEvent, [ event ], "'dispatchEvent' without illegal event type should trigger 'IllegalArgumentException'" );
     }
 	
     @Test( "Test dispatch method with arguments" )
@@ -44,9 +51,14 @@ class MonoTypeClosureDispatcherTest
 		Assert.equals( this._target, this._lastReceivedEvent.target, "targets should be the same" );
 		Assert.equals( "event", this._lastReceivedEvent.type, "types should be the same" );
 		Assert.isInstanceOf( this._lastReceivedEvent, BasicEvent, "event received should be an instance of 'BasicEvent'" );
+		
+		this._lastReceivedEvent = null;
+		this._dispatcher.removeEventListener( this._onCallback );
+		this._dispatcher.dispatchEvent( event );
+		Assert.isNull( this._lastReceivedEvent, "event should not be dispatched" );
     }
 	
-	 @Test( "Test dispatch method without arguments" )
+	@Test( "Test dispatch method without arguments" )
     public function testDispatchMethodWithoutArguments() : Void
     {
 		#if ( haxe_ver >= "3.3" )
@@ -60,13 +72,54 @@ class MonoTypeClosureDispatcherTest
 		Assert.methodCallThrows( IllegalArgumentException, this._dispatcher, this._dispatcher.dispatchEvent, [], "'dispatchEvent' without argument should trigger 'IllegalArgumentException'" );
 		
 		#end
+		
+		this._lastReceivedEvent = null;
+		this._dispatcher.removeEventListener( this._onCallback );
+		var event = new BasicEvent( "event", this._target );
+		this._dispatcher.dispatchEvent( event );
+		
+		Assert.isNull( this._lastReceivedEvent, "event should not be dispatched" );
     }
+	
+	@Test( "Test isEmpty" )
+	public function testIsEmpty() : Void
+	{
+		Assert.isFalse( this._dispatcher.isEmpty(), "'isEmpty' should return false" );
+		this._dispatcher.removeEventListener( this._onAnotherCallback );
+		Assert.isFalse( this._dispatcher.isEmpty(), "'isEmpty' should return false" );
+		this._dispatcher.removeEventListener( this._onCallback );
+		Assert.isTrue( this._dispatcher.isEmpty(), "'isEmpty' should return true" );
+	}
+	
+	@Test( "Test removeAllListeners" )
+	public function testRemoveAllListeners() : Void
+	{
+		this._dispatcher.addEventListener( this._onAnotherCallback );
+		
+		Assert.isFalse( this._dispatcher.isEmpty(), "'isEmpty' should return false" );
+		
+		var event = new BasicEvent( "event", this._target );
+		this._dispatcher.dispatchEvent( event );
+		
+		Assert.isNotNull( this._lastReceivedEvent, "event should not be dispatched" );
+		
+		this._lastReceivedEvent = null;
+		this._dispatcher.removeAllListeners();
+		this._dispatcher.dispatchEvent( event );
+		
+		Assert.isTrue( this._dispatcher.isEmpty(), "'isEmpty' should return true" );
+		Assert.isNull( this._lastReceivedEvent, "event should not be dispatched" );
+	}
 	
 	private function _onCallback( e : BasicEvent ) : Void
 	{
 		this._lastReceivedEvent = e;
 	}
 	
+	private function _onAnotherCallback( e : BasicEvent ) : Void
+	{
+		this._lastReceivedEvent = e;
+	}
 }
 
 private class MockTarget
