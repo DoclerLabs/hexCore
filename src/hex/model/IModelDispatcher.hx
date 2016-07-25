90b7f12413ba45daaba3468926e54d04688ba01c
francis.bourre <francis_bourre@me.com>
2016-07-25 07:29:16 +0200

    Fix for deprecated new operator in typedef definition (>=3.3)

--- src/hex/model/IModelDispatcher.hx
+++ src/hex/model/IModelDispatcher.hx
@@ -1,11 +1,19 @@
 package hex.model;
 
+#if ( haxe_ver >= "3.3" )
+import haxe.Constraints.Constructible;
+#end
+
 /**
  * @author Francis Bourre
  */
 typedef IModelDispatcher<ListenerType> = 
 {
+	#if ( haxe_ver >= "3.3" )
+	> Constructible<Void->Void>,
+	#else
 	public function new() : Void;
+	#end
 	
 	public function addListener( listener : ListenerType ) : Bool;
 
