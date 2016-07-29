0eded2a350fd6a1234a3f7164d7da3b2ab9ae406
francis.bourre <francis_bourre@me.com>
2016-07-29 11:27:34 +0200

    IModelDispatcher fix for <3.3

--- src/hex/model/IModelDispatcher.hx
+++ src/hex/model/IModelDispatcher.hx
@@ -5,6 +5,10 @@ package hex.model;
  */
 typedef IModelDispatcher<ListenerType> = 
 {
+	#if ( haxe_ver < "3.3" )
+	public function new() : Void;
+	#end
+	
 	public function addListener( listener : ListenerType ) : Bool;
 
 	public function removeListener( listener : ListenerType ) : Bool;
