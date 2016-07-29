c09f593001de71f3c83e009d075278b3d3146fe6
francis.bourre <francis_bourre@me.com>
2016-07-29 06:36:23 +0200

    removed deprecated declaration

--- src/hex/model/IModelDispatcher.hx
+++ src/hex/model/IModelDispatcher.hx
@@ -1,20 +1,10 @@
 package hex.model;
 
-/*#if ( haxe_ver >= "3.3" )
-import haxe.Constraints.Constructible;
-#end*/
-
 /**
  * @author Francis Bourre
  */
 typedef IModelDispatcher<ListenerType> = 
 {
-	/*#if ( haxe_ver >= "3.3" )
-	> Constructible<Void->Void>,
-	#else*/
-	public function new() : Void;
-	//#end
-	
 	public function addListener( listener : ListenerType ) : Bool;
 
 	public function removeListener( listener : ListenerType ) : Bool;
