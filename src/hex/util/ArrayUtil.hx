2a171e7dd44059a23c75b736d83ca68f7f2a0941
francis.bourre <francis_bourre@me.com>
2016-08-18 10:43:16 +0200

    short lambda on Array.countWhen

--- src/hex/util/ArrayUtil.hx
+++ src/hex/util/ArrayUtil.hx
@@ -1,5 +1,6 @@
 package hex.util;
 
+import haxe.macro.Expr;
 import hex.error.PrivateConstructorException;
 
 /**
@@ -39,12 +40,6 @@ class ArrayUtil
 			if ( f( e ) ) ff ( e, i );
 		}
 	}
-	public static function countWhen<T>( a : Array<T>, f : T -> Bool ) : Int
-	{
-		var c = 0;
-		for ( e in a ) if ( f( e ) ) c++;
-		return c;
-	}
 	
 	public static function doOnFirstWhen<T>( a : Array<T>, f : T -> Bool, ff : T -> Int -> Void ) : Void
 	{
@@ -66,6 +61,74 @@ class ArrayUtil
 		{
 			var e = a[ i ];
 			f ( e, i );
+		}
+	}
+	
+	static function arrowDecompose( f: Expr ) : { left: Expr, right: Expr }
+	{
+		var l: Expr = null;
+		var r: Expr = null;
+		
+		switch( f.expr )
+		{
+			case EBinop( OpArrow, e1, e2 ):
+			{
+				l = e1;
+				r = e2;
+			}
+			
+			default: r = f;
+		}
+		
+		return { left: l, right: r };
+	}
+	
+	static function leftName( e : Expr )
+	{
+		return leftNames( e )[ 0 ];
+	}
+	
+	static function leftNames( L : Expr, min : Int = 1 )
+	{
+		var names : Array<String> =
+			if ( L != null )
+			{
+				switch( L.expr )
+				{
+					case EConst( CIdent( s ) ): [ s ];
+					
+					case EArrayDecl( els ): 
+						els.map( function( e )
+							return switch( e.expr )
+							{
+								case EConst( CIdent( s ) ): s; 
+								default: '_';
+							});
+							
+					case _: [];
+				}
+			}
+			else [];
+		
+		var t = '';
+		while ( names.length < min ) names.push( t += '_' );
+		return names;
+	}
+	
+	macro public static function countWhen<T>( a : ExprOf<Array<T>>, f : ExprOf<Bool> ) : Expr
+	{
+		var ad 		= arrowDecompose( f );
+		var lName 	= leftName( ad.left );
+		var rVal 	= ad.right;
+
+		return macro
+		{
+			var c = 0;
+			for ( $i{lName} in $a )
+			{
+				if ($rVal) c += 1;
+			}
+			c;
 		}
 	}
 }
