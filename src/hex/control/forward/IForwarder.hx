23d770771d0364d243400aa5fab2d7b438e55450
Francis Bourre <francis_bourre@me.com>
2017-01-25 07:34:54 +0100

    forwarder from mdvc added

---
+++ src/hex/control/forward/IForwarder.hx
@@ -0,0 +1,13 @@
+package hex.control.forward;
+
+/**
+ * @author Francis Bourre
+ */
+#if !macro
+@:remove
+@:autoBuild( hex.control.forward.ForwarderBuilder.build() )
+#end
+interface IForwarder 
+{
+  
+}
