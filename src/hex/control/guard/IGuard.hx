3da13860450c97f104ec760ca9c2750f2bf159ad
Francis Bourre <francis_bourre@me.com>
2017-02-03 10:44:37 +0100

    IGuard implements IInjectorContainer by default.

--- src/hex/control/guard/IGuard.hx
+++ src/hex/control/guard/IGuard.hx
@@ -1,10 +1,12 @@
 package hex.control.guard;
 
+import hex.di.IInjectorContainer;
+
 /**
  * ...
  * @author Francis Bourre
  */
-interface IGuard
+interface IGuard extends IInjectorContainer
 {
     function approve() : Bool;
 }
