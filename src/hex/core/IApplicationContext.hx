1631191e48cd4a06cbca3fdeba259033b508c1f1
Francis Bourre <francis_bourre@me.com>
2018-03-12 13:47:41 +0100

    DomainDispatcher stuff removed

--- src/hex/core/IApplicationContext.hx
+++ src/hex/core/IApplicationContext.hx
@@ -2,7 +2,6 @@ package hex.core;
 
 import hex.di.IDependencyInjector;
 import hex.domain.Domain;
-import hex.event.MessageType;
 import hex.module.IContextModule;
 
 /**
@@ -13,8 +12,6 @@ interface IApplicationContext extends IContextModule
 	function getName() : String;
 	
 	function getDomain() : Domain;
-
-	function dispatch( messageType : MessageType, ?data : Array<Dynamic> ) : Void;
 	
 	function getCoreFactory() : ICoreFactory;
 	
