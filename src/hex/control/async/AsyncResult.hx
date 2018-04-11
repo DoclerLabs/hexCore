952aac0f092c1b1db0b3817d19731c710346b51c
Francis Bourre <francis_bourre@me.com>
2018-04-11 10:04:50 +0200

    tink.CoreAPI Error replaces Exception

--- src/hex/control/async/AsyncResult.hx
+++ src/hex/control/async/AsyncResult.hx
@@ -1,25 +1,33 @@
 package hex.control.async;
 
-import hex.control.async.AsyncCallback;
+using tink.CoreApi;
 
 /**
  * ...
  * @author Francis Bourre
  */
-abstract AsyncResult<ResultType>( Result<ResultType> ) from ( Result<ResultType> )
+abstract AsyncResult<ResultType, Failure>( Result<ResultType, Failure> ) from ( Result<ResultType, Failure> )
 {
 	inline function new( r )
 	{
 		this = r;
 	}
 
-	@:from public static inline function completed<ResultType>( result : ResultType ) : AsyncResult<ResultType> 
+	@:from public static inline function completed<ResultType, Failure>( result : ResultType ) : AsyncResult<ResultType, Failure> 
 	{
 		return new AsyncResult( Result.DONE( result ) );
 	}
 	
-	@:from public static inline function failed<ResultType>( error : hex.error.Exception ) : AsyncResult<ResultType> 
+	@:from public static inline function failed<ResultType, Failure>( error : Failure ) : AsyncResult<ResultType, Failure> 
 	{
-		return new AsyncResult( Result.FAILED( error) );
+	return new AsyncResult( Result.FAILED( error ) );
 	}
+}
+
+enum Result<T, E>
+{
+	WAITING;
+	DONE( result : T );
+	FAILED( e : E );
+	CANCELLED;
 }
--- src/hex/error/UnsupportedOperationException.hx
+++ src/hex/error/UnsupportedOperationException.hx
@@ -1,16 +1,14 @@
 package hex.error;
 
 import haxe.PosInfos;
+using tink.CoreApi;
 
 /**
  * ...
  * @author Francis Bourre
  */
 @IgnoreCover
-class UnsupportedOperationException extends Exception
+class UnsupportedOperationException extends Error
 {
-    public function new ( message : String, ?posInfos : PosInfos )
-    {
-        super( message, posInfos );
-    }
+    public function new ( message : String, ?posInfos : PosInfos ) super( code, message, pos );
 }
